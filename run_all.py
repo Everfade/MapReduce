b2b6456a2e61f064f80a71899d1bfdc8626d44a8
GreatPotatoCat <43379461+NiklasSchuster@users.noreply.github.com>
2024-04-23 20:37:29 +0200

    changed runner.py

--- run_all.py
+++ run_all.py
@@ -3,22 +3,27 @@ import time
 
 start_time = time.time()
 
-# python preprocessing.py reviews_devset.json > values_for_chi2.txt
-# python reviewcounter.py reviews_devset.json > category_counters.txt
+# python preprocessing.py reviewscombined.json > values_for_chi2.txt
+# python reviewcounter.py reviewscombined.json > category_counters.txt
 # python calculate_chi.py values_for_chi2.txt > output.txt
 
 start_time = time.time()
 preprocessing = "python preprocessing.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r \
-    hadoop hdfs:///user/dic24_shared/amazon-reviews/full/reviews_devset.json > values_for_chi2.txt"
+    hadoop hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json > values_for_chi2.txt"
 
 review_counter = "python reviewcounter.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r \
-    hadoop hdfs:///user/dic24_shared/amazon-reviews/full/reviews_devset.json > category_counters.txt"
+    hadoop hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json > category_counters.txt"
 
 calculate_chi = "python calculate_chi.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r \
-    values_for_chi2 > output.txt"
+    values_for_chi2.txt > output.txt"
 
 exit_code = os.system(preprocessing)
 print("--- %s minutes ---" % ((time.time() - start_time)/60))
+exit_code = os.system(review_counter)
+print("--- %s minutes ---" % ((time.time() - start_time)/60))
+exit_code = os.system(calculate_chi)
+print("--- %s minutes ---" % ((time.time() - start_time)/60))
+
 if exit_code == 0:
     print("Command executed successfully")
 else:
